d652afffbb3684395e05ed88686a154d8fd20f6d
Akhila Chirumamilla <83582488+Akhila123-coder@users.noreply.github.com>
2021-07-18 19:28:01 +0530

    Add files via upload

---
+++ pps lab/reversing.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char ch1,ch2,ch3;
+	printf("\nEnter three charcaters: ");
+	scanf("%c\n",&ch1);
+	scanf("%c\n",&ch2);
+	scanf("%c",&ch3);
+	printf("\n ch1=%c ch2=%c ch3=%c ",ch3,ch2,ch1);
+	return 0;
+}
