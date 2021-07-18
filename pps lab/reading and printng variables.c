e352002ff01a6a370b074bb64467ba830250f63e
Akhila Chirumamilla <83582488+Akhila123-coder@users.noreply.github.com>
2021-07-18 20:43:48 +0530

    Add files via upload

---
+++ pps lab/reading and printng variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	float n1;
+	char ch;
+	printf("Enter the data:");
+	scanf("%d",&num);
+	printf("\n number is %d",num);
+	scanf("%f",&n1);
+	printf("\n floating number is %f",n1);
+	scanf("%c",&ch);
+	printf("\n character is %c",ch);
+	
+}
