fa91b09835403964eebcc604bf01614e830780e4
Akhila Chirumamilla <83582488+Akhila123-coder@users.noreply.github.com>
2021-07-18 20:03:21 +0530

    Add files via upload

---
+++ pps lab/size check.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("\n Size of int is %d bytes",sizeof(int));
+	printf("\n Size of short int %d bytes",sizeof(short int));
+	printf("\n size of float is %d bytes",sizeof(float));
+	printf("\n size of character is %d bytes ",sizeof(char));
+	printf("\n size of double is %d bytes",sizeof(double));
+}
