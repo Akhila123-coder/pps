6a2d070b11c3b492f01d506a4502b46b506da42d
Akhila Chirumamilla <83582488+Akhila123-coder@users.noreply.github.com>
2021-07-19 22:35:12 +0530

    Add files via upload

---
+++ pps lab/47.ascii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the input ");
+	scanf("%c",&a);
+	printf("\n%d",a);
+	(a>48&&a<57)? printf("\na is a number"):printf("a is not a number");
+}
