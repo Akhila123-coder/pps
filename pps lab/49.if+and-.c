9c65ea2ae41cb6e136070a5f80c5038ce30c5815
Akhila Chirumamilla <83582488+Akhila123-coder@users.noreply.github.com>
2021-07-19 22:38:56 +0530

    Add files via upload

---
+++ pps lab/49.if+and-.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter the input ");
+	scanf("%d",&num);
+	if(num>0)
+	printf("the num =%d is positive",num);
+	else
+	printf("the num=%d is negative",num);
+}
