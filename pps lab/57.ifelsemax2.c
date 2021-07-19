c5da2dbda77836ce627f8521924565ceac4133c9
Akhila Chirumamilla <83582488+Akhila123-coder@users.noreply.github.com>
2021-07-19 22:54:39 +0530

    Add files via upload

---
+++ pps lab/57.ifelsemax2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter the input ");
+	scanf("%d",&num);
+	if(num%2==0)
+	printf("the number=%d is even",num);
+	else
+	printf("the number=%d is odd",num);
+}
+© 2021 GitHub, Inc.
