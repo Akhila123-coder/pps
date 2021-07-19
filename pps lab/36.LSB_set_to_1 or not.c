98be8aea8ce725aac95131337e8304ddea9f3e4b
Akhila Chirumamilla <83582488+Akhila123-coder@users.noreply.github.com>
2021-07-19 22:11:33 +0530

    Add files via upload

---
+++ pps lab/36.LSB_set_to_1 or not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("\n enter the number ");
+	scanf("%d",&a);
+	if(a&1)
+	printf("\n given number's LSB is set to 1");
+	else
+	printf("\n given number's LSB is set to 0");
+	}
+	
+
+
